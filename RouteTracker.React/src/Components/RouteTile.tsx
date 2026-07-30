ff7efadb891496589cbffcdfd1cfd678eff87d69
gcwillis <gcwillis.15@gmail.com>
2026-07-30 16:56:59 -0400

    added tailwind and created a new tile component

---
+++ RouteTracker.React/src/Components/RouteTile.tsx
@@ -0,0 +1,27 @@
+type TileProps = {
+    grade: string
+    color: string
+    date: string
+    setter: string
+    location: string
+}
+
+function RouteTile(props: TileProps) {
+    return (
+        <div className="mt-5 ml-5 box-border flex h-[5vw] w-[15vw] overflow-hidden rounded-2xl border-2 border-[#16171d] bg-gray-800 transition-colors hover:border-white">
+            <div
+                className="flex h-full w-[5vw] items-center justify-center rounded-r-2xl pt-1.5"
+                style={{ backgroundColor: props.color }}
+            >
+                <h2 className="h-10 w-15 rounded-md bg-black/40 pt-1.5">{props.grade}</h2>
+            </div>
+            <div className="ml-3 pt-1.5 text-left text-white">
+                <h3>Date - {props.date}</h3>
+                <h3>Setter - {props.setter}</h3>
+                <h3>Location - {props.location}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default RouteTile
--- RouteTracker.React/src/Pages/HomePage.tsx
+++ RouteTracker.React/src/Pages/HomePage.tsx
@@ -1,7 +1,27 @@
+import RouteTile from "../Components/RouteTile"
+
+const red = "#BD0000"
+const orange = "#DE4A00"
+const yellow = "#FFEE14"
+const green = "#1D8216"
+const blue = "#0072C4"
+const purple = "#4E1C8A"
+const pink = "#FF00FF"
+const white = "#F2F2F2"
+const black = "#212121"
 
 function HomePage() {
     return (
         <>
+            <RouteTile grade="5.7" color={red} date="12/12/2026" setter="bob" location="Line 40" />
+            <RouteTile grade="5.8" color={orange} date="12/12/2026" setter="bob" location="Line 40" />
+            <RouteTile grade="5.9" color={yellow} date="12/12/2026" setter="bob" location="Line 40" />
+            <RouteTile grade="5.10" color={green} date="12/12/2026" setter="bob" location="Line 40" />
+            <RouteTile grade="5.11" color={blue} date="12/12/2026" setter="bob" location="Section 4" />
+            <RouteTile grade="5.12" color={purple} date="12/12/2026" setter="bob" location="Line 40" />
+            <RouteTile grade="5.12a" color={pink} date="12/12/2026" setter="bob" location="Line 40" />
+            <RouteTile grade="5.12b" color={white} date="12/12/2026" setter="bob" location="Line 40" />
+            <RouteTile grade="5.13c" color={black} date="12/12/2026" setter="bob" location="Line 40" />
         </>
     )
 }
