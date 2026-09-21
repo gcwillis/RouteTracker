a9f887c303c7e3261f8ebb676eba7af0938e21b3
gcwillis <gcwillis.15@gmail.com>
2026-09-21 17:55:38 -0400

    Fixed RouteTile component styling

--- RouteTracker.React/src/Components/RouteTile.tsx
+++ RouteTracker.React/src/Components/RouteTile.tsx
@@ -3,7 +3,7 @@ import type { TileProps } from "../utils/types"
 function RouteTile(props: TileProps) {
     return (
         <div 
-            className="mt-5 ml-5 box-border flex h-10 w-fit overflow-hidden rounded-md select-none shadow-md shadow-black/20
+            className="mt-5 ml-5 box-border flex h-10 w-150 overflow-hidden rounded-md select-none shadow-md shadow-black/20
             border-2 border-black/20 bg-gray-800 transition-colors hover:border-blue-900 active:border-white">
             <div
                 className="flex h-full w-[5vw] items-center justify-center rounded-r-sm pt-1.5"
@@ -12,9 +12,9 @@ function RouteTile(props: TileProps) {
                 <h2 className="h-10 w-15 rounded-md bg-black/40 pt-1.5">{props.grade}</h2>
             </div>
             <div className="ml-3 flex gap-4 text-left text-white">
-                <h3 className="bg-black/20 rounded-sm p-1.5"><b>Location</b> - {props.location}</h3>
-                <h3 className="bg-black/20 rounded-sm p-1.5"><b>Set by</b> - {props.setter}</h3>
-                <h3 className="bg-black/20 rounded-sm p-1.5"><b>Set on</b> - {props.date}</h3>
+                <h3 className="bg-black/20 w-40 rounded-sm p-1.5"><b>Location</b> - {props.location}</h3>
+                <h3 className="bg-black/20 w-40 rounded-sm p-1.5"><b>Set by</b> - {props.setter}</h3>
+                <h3 className="bg-black/20 w-40 rounded-sm p-1.5"><b>Set on</b> - {props.date}</h3>
             </div>
         </div>
     )
